39c653e227298e10b0edddef52661d7d46d5180f
Mohamed Ismail <mihafez.mi@gmail.com>
2015-12-13 19:53:22 +0200

    my modules first version

---
+++ reg file.v
@@ -0,0 +1,17 @@
+module RegFile(ra1, rd1 , ra2 , rd2 , clk , RegWrite , wa ,wd );
+input[4:0] ra1;
+output[31:0] rd1;
+input[4:0] ra2;
+output[31:0] rd2;
+input clk;
+input werf ;
+input[4:0] wa;
+input[31:0] wd;
+reg [31:0] registers[31:0];
+assign rd1 = registers[ra1];
+assign rd2 = registers[ra2];
+
+always@ ( posedge clk )
+    if (RegWrite)
+        registers[wa] <= wd;
+endmodule
